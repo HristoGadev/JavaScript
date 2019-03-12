eda69274512131264fdc186225021f139d52f2d1
HristoGadev <40789003+HristoGadev@users.noreply.github.com>
2019-03-12 21:46:22 +0200

    Delete Sort an Array by 2 Criteria.js

--- JS Fundamentals/JavaScript Syntax and Operators/Sort an Array by 2 Criteria.js
+++
@@ -1,9 +0,0 @@
-function solve(arr) {
-    let sortedArr =arr.sort().sort((a, b) => a.length > b.length) 
-   
-    console.log(sortedArr.join('\n'));
-}
-solve(['alpha',
-    'beta',
-    'gamma']
-)
